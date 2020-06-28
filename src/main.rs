f8e9b85e20558130d2d9ea04f04d17de056b6f60
Roman Khomenko <romankhomenko1995@gmail.com>
2020-06-28 20:37:10 +0300

    Simple directory traversal added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn visit_dir(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.is_dir() {
+                visit_dir(&path).unwrap();
+            } else {
+                println!("{}", path.to_str().unwrap());
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    visit_dir(Path::new("/home/rk/Documents")).unwrap();
 }
