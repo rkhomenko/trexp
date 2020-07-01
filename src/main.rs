f4f506487f32ca7c71a89c01088e467342a4c84e
Roman Khomenko <romankhomenko1995@gmail.com>
2020-07-01 18:52:26 +0300

    Added directory size calculation

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
-fn visit_dir(dir: &Path) -> io::Result<()> {
+fn visit_dir(dir: &Path) -> io::Result<u64> {
+    let mut dir_size: u64 = 0;
+
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
 
             if path.is_dir() {
-                visit_dir(&path)?;
+                dir_size += visit_dir(&path)?;
             } else {
-                println!("{}: size {}", path.display(), entry.metadata()?.len());
+                let file_size = entry.metadata()?.len();
+                dir_size += file_size;
+                println!("{}, size {}", path.display(), file_size);
             }
         }
+
+        println!("{}, size {}", dir.display(), dir_size);
     }
-    Ok(())
+
+    Ok(dir_size)
 }
 
 fn main() {
