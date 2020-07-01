68423150f07cf50237e169c3e4d7cdb5f0c9557d
Roman Khomenko <romankhomenko1995@gmail.com>
2020-07-01 20:32:04 +0300

    Added FileSearcher data structure

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,76 @@
 use std::fs;
+use std::fs::Metadata;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-fn visit_dir(dir: &Path) -> io::Result<u64> {
-    let mut dir_size: u64 = 0;
+struct FileInfo {
+    path_buf: PathBuf,
+    meta: Metadata,
+}
 
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
+impl FileInfo {
+    fn new(path: &Path, meta: Metadata) -> FileInfo {
+        FileInfo {
+            path_buf: path.to_path_buf(),
+            meta,
+        }
+    }
+
+    fn get_path(&self) -> &Path {
+        &self.path_buf.as_path()
+    }
+
+    fn get_size(&self) -> u64 {
+        self.meta.len()
+    }
+}
+
+struct FileSearcher {
+    files: Vec<FileInfo>,
+}
+
+impl FileSearcher {
+    fn new() -> FileSearcher {
+        FileSearcher {
+            files: Vec::<FileInfo>::new(),
+        }
+    }
+
+    fn visit_directory(files: &mut Vec<FileInfo>, path: &Path) -> io::Result<()> {
+        for entry in fs::read_dir(path)? {
             let entry = entry?;
             let path = entry.path();
 
             if path.is_dir() {
-                dir_size += visit_dir(&path)?;
+                FileSearcher::visit_directory(files, &path)?;
             } else {
-                let file_size = entry.metadata()?.len();
-                dir_size += file_size;
-                println!("{}, size {}", path.display(), file_size);
+                files.push(FileInfo::new(&path, fs::metadata(&path)?))
             }
         }
-
-        println!("{}, size {}", dir.display(), dir_size);
+        Ok(())
     }
 
-    Ok(dir_size)
+    fn search(&mut self, path: &Path) -> io::Result<()> {
+        FileSearcher::visit_directory(&mut self.files, &path)?;
+        Ok(())
+    }
+
+    fn sort_by_size(&mut self) {
+        self.files
+            .sort_by(|a, b| a.meta.len().cmp(&b.meta.len()).reverse())
+    }
+
+    fn get(&self) -> &Vec<FileInfo> {
+        &self.files
+    }
 }
 
 fn main() {
-    visit_dir(Path::new("/home/rk/Documents")).unwrap();
+    let mut searcher = FileSearcher::new();
+    searcher.search(Path::new("/home/rk/Documents")).unwrap();
+    searcher.sort_by_size();
+
+    for file in searcher.get() {
+        println!("{}, size: {}", file.get_path().display(), file.get_size())
+    }
 }
